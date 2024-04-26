9ad125474e7cc8da042e84593d2be93897ebf27f
Carlos Viteri <carlos.viteri@herodigital.com>
2024-04-26 14:33:56 -0500

    add gallery scripts

--- blocks/gallery/gallery.js
+++ blocks/gallery/gallery.js
@@ -1,8 +1,37 @@
 export default function decorate(block) {
-  const container = block.firstElementChild;
-  container.classList.add('gallery-container');
+  block.innerHTML = `
+    <div id="olapic_block">
+      <div id="olapic_specific_widget"></div>
+    </div>
+  `;
 
-  [...container.children].forEach((child) => {
-    child.classList.add('gallery-item');
+  const olapicBlock = document.getElementById('olapic_block');
+  function loadOlapicScript() {
+    const olapicIsLoaded = document.querySelectorAll('.olapic-script');
+    if (olapicIsLoaded.length > 0) {
+      return;
+    }
+    const olapicScript = document.createElement('script');
+    olapicScript.src = 'https://photorankstatics-a.akamaihd.net/743d2e78a76dedeb07e0745158547931/static/frontend/latest/build.min.js';
+    olapicScript.setAttribute('data-olapic', 'olapic_specific_widget');
+    olapicScript.setAttribute(
+      'data-instance',
+      '2827c532b51f49bfa3711fdf2445c908',
+    );
+    olapicScript.setAttribute(
+      'data-apikey',
+      '1c1b740977981200d49891d310b5be90cce45e09eaa3c0c77115019dd67b3cd2',
+    );
+    olapicScript.setAttribute('class', 'olapic-script');
+    olapicBlock.appendChild(olapicScript);
+  }
+  const olapicBlockObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        loadOlapicScript();
+        olapicBlockObserver.unobserve(entry.olapicBlock);
+      }
+    });
   });
+  olapicBlockObserver.observe(olapicBlock);
 }
