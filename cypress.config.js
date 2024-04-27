9c4895249c9adfeaafe1b777c746b44b5793260d
zephanhazell <2414175+zephanhazell@users.noreply.github.com>
2024-04-26 21:54:32 -0600

    tweaking cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   projectId: 'b999ha',
   e2e: {
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
   },
 });
